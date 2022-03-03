baad60270ac5032e5b179e6acef246dcaf972a1f
Ayi <ayianjani774@gmail.com>
2022-03-03 10:41:57 +0700

    membuat view contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Contact Us</h2>
+    <ul class="list-group">
+        <li class="list-group-item"><a href="https://www.instagram.com/educastudio/">Instagram</a></li>
+        <li class="list-group-item"><a href="https://www.facebook.com/educastudio">Facebook</a></li>
+        <li class="list-group-item"><a href="https://twitter.com/educastudio">Twitter</a></li>
+
+    </ul>
+</div>
+@endsection
