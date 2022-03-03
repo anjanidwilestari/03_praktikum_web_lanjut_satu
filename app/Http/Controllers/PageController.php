5ca7bfd28540ae6f1d4fe79b4eaae26435a7f523
Ayi <ayianjani774@gmail.com>
2022-03-03 10:04:33 +0700

    Kode Program Pertama

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+    public function product()
+    {
+        $product=['Marble_Piano', 'Marble_Tangram', 'Marbel_Dinosaurus'];
+        return view('product', ['product' => $product]);
+    }
+}
