79d4a6789eb6bbaf97c0184f55884d05f0a34958
Ayi <ayianjani774@gmail.com>
2022-03-03 10:05:47 +0700

    Kode Program pertama

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 <div class="container">
-    <h2>Our Product</h2>
-    <ul class="list-group">
-        @foreach ($product as $item)
-            <li class="list-group-item"><a href="/product">{{ $item }}</a></li>
-        @endforeach
-    </ul>
+    <h2>Berita</h2>
+    
 </div>
 @endsection
