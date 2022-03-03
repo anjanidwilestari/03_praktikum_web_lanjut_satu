62d7afb1315d55ab2790e61362b08b9c879651e5
Ayi <ayianjani774@gmail.com>
2022-03-03 10:28:43 +0700

    membuat view about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 <div class="container">
-    <h2>Berita {{ $id }}</h2>
+    <h2>About Company</h2>
+    <p>Owner by Anjani Dwilestari</p>
+    <p>Student of Politeknik Negeri Malang</p>
+    <p>ID 2041720180 | TI-2E</p>
     
 </div>
 @endsection
